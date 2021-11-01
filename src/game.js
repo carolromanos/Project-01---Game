c64584966e7b305e5bc57cd415d8b1c7a35115b1
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-01 13:09:11 +0100

    screens with player and shoot

--- src/game.js
+++ src/game.js
@@ -17,11 +17,62 @@ class Game {
     
 
     // Create a new player for the current game
-    
     this.player = new Player (this.canvas, 30, "blue")
     this.player.draw()
     
+  
+
+    addEventListener(`click`, (event) =>{
+        const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+        const speed = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        //Create projectiles upon click
+        this.projectiles.push(new Projectile (this.ctx, this.canvas.width / 2, this.canvas.height / 2, 5,`red`, speed))
+        console.log(this.projectiles)
+
+    })
+    this.startLoop();
+
+    
   }
+/*   animateProjectile(){
+    window.requestAnimationFrame(animateProjectile())
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+    this.player.draw()
+    this.projectiles.forEach ((projectile) =>{
+        projectile.update()
+        projectile.draw()
+    })
+  } */
+
+  startLoop() {
+    const loop = () => {
+
+      this.player.draw()
+
+      this.projectiles.forEach ((projectile) =>{
+        projectile.update()
+      
+    })
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+    this.player.draw()
+
+    this.projectiles.forEach ((projectile) =>{
+    
+        projectile.draw()
+    })
+
+
+      window.requestAnimationFrame(loop);
+    };
+
+    window.requestAnimationFrame(loop);
+  }
+
+  
 
 }
   
